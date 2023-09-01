d7257af766d3be34e24f3bedb08c470798ae6ea2
Kirill <kertis84@gmail.com>
2023-09-01 20:46:25 +0300

    main commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div>
+        Бе
+    </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/__tests__/HelloWorld.spec.ts
@@ -0,0 +1,11 @@
+import { describe, it, expect } from 'vitest'
+
+import { mount } from '@vue/test-utils'
+import App from '../../App.vue'
+
+describe('HelloWorld', () => {
+  it('renders properly', () => {
+    const wrapper = mount(App, { props: { msg: 'Hello Vitest' } })
+    expect(wrapper.text()).toContain('Hello Vitest')
+  })
+})
